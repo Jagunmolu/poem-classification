67132aaaf3c7f490599df0517dab18c43fa61bf2
Jagunmolu <kolawolejonathan@gmail.com>
2024-05-24 23:48:04 +0100

    removed dead code and commented code

--- app.py
+++ app.py
@@ -1,27 +1,9 @@
 import streamlit as st
-# import pandas as pd
-# import numpy as np
-# from prediction import predict
-# from models import model, confusion_matrix
-# from sklearn.metrics import ConfusionMatrixDisplay, classification_report
 from models import Models
 
 st.title("Classifying Poems")
-# st.markdown("Toy model to play to classify iris flowers into setosa, versicolor, virginica")
-
-# st.header("Plant Features")
-# col1, col2 = st.columns(2)
-# with col1:
-#     st.text("Sepal characteristics")
-#     sepal_l = st.slider("Sepal lenght (cm)", 1.0, 8.0, 0.5)
-#     sepal_w = st.slider("Sepal width (cm)", 2.0, 4.4, 0.5)
-# with col2:
-#     st.text("Pepal characteristics")
-#     petal_l = st.slider("Petal lenght (cm)", 1.0, 7.0, 0.5)
-#     petal_w = st.slider("Petal width (cm)", 0.1, 2.5, 0.5)
 
 text = st.text_input("Enter the text to classify.")
-# text = st.text_area("Enter the text to classify.")
 choice = st.selectbox("Pick your preferred model.", ["random forest", "svm", "xgboost", "decision tree", "naive bayes multinomial", "naive bayes complement", "knn"])
 
 model = Models(choice)
@@ -32,12 +14,6 @@ predict_button = st.button("Predict Poem Category")
 if predict_button:
     result = model.clf.predict([text])
     st.text(result[0])
-    # st.pyplot(ConfusionMatrixDisplay(confusion_matrix()).plot())
-    # st.pyplot(confusion_matrix())
-    # st.write(confusion_matrix)
-    # confusion_matrix
     st.image("temp.png")
     st.code(model.metrics())
-    # st.text(model.metrics())
-    # st.metric(model.metrics())
     
--- models.py
+++ models.py
@@ -1,8 +1,5 @@
-import numpy as np
 import pandas as pd
-# import joblib
 import matplotlib.pyplot as plt
-# import seaborn as sns
 import string
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import FunctionTransformer
@@ -10,68 +7,12 @@ from sklearn.pipeline import make_pipeline
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import ConfusionMatrixDisplay, classification_report, confusion_matrix
 from sklearn.svm import SVC
-# from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-# from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import GaussianNB, MultinomialNB, ComplementNB
+from sklearn.naive_bayes import MultinomialNB, ComplementNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from xgboost import XGBClassifier
 
-# def remove_punctuations(text):
-#     punctuations = string.punctuation + "0123456789"
-#     res = []
-#     for _ in text:
-#         res.append("".join([char for char in _ if char not in punctuations]))
-#     return res
-
-# train = pd.read_csv(r"data/Poem_classification_train_data.csv")
-# print(train.shape)
-# train.head()
-# train.dropna(inplace=True)
-# train.shape
-# test = pd.read_csv(r"data/Poem_classification_test_data.csv")
-# print(test.shape)
-# test.head()
-
-# cv = CountVectorizer(stop_words="english")
-
-# train["Poem"] = train["Poem"].apply(_punctuations)
-# test["Poem"] = test["Poem"].apply(_punctuations)
-# print(train.head())
-
-# X_train = train["Poem"]
-# y_train = train["Genre"]
-
-# X_test =test["Poem"]
-# y_test = test["Genre"]
-
-# X_train_vect = cv.fit_transform(X_train).toarray()
-# X_test_vect = cv.transform(X_test).toarray()
-
-# X_train_df = pd.DataFrame(X_train_vect, columns=cv.get_feature_names_out())
-# X_test_df = pd.DataFrame(X_test_vect, columns=cv.get_feature_names_out())
-# print(X_train_df.shape)
-# X_train_df.head()
-
-# clf = make_pipeline(FunctionTransformer(remove_punctuations), CountVectorizer(stop_words="english"), RandomForestClassifier(random_state=42))
-# clf.fit(X_train, y_train)
-# print(classification_report(y_test, clf.predict(X_test)))
-# cm = ConfusionMatrixDisplay.from_estimator(clf, X_test, y_test)
-
-# cm = confusion_matrix(y_test, clf.predict(X_test))
-# display = ConfusionMatrixDisplay(cm).plot()
-
-# joblib.dump(clf, "rf_model.sav")
-
-# def model():
-#     return clf
-
-# def confusion_matrix():
-    # print(type(cm))
-    # return cm
-    # return display
 
 class Models:
     def __init__(self, model):
@@ -87,19 +28,10 @@ class Models:
         self.pred = None
         self.model = model
 
-    # def feature_engineering(self):
-    #     X_train = self.train["Poem"]
-    #     y_train = self.train["Genre"]
-        
-    #     X_test = self.test["Poem"]
-    #     y_test = self.test["Genre"]
-    #     return (X_train, y_train), (X_test, y_test)
         
     def model_init(self):
         if self.model == "random forest":
             self.clf = make_pipeline(FunctionTransformer(self.remove_punctuations), CountVectorizer(stop_words="english"), RandomForestClassifier(random_state=42))
-        # elif self.model == "svm":
-        #     self.clf = make_pipeline(FunctionTransformer(self.remove_punctuations), CountVectorizer(stop_words="english"), MinMaxScaler(feature_range=(-1, 1)), SVC(random_state=42))
         elif self.model == "svm":
             self.clf = make_pipeline(FunctionTransformer(self.remove_punctuations), CountVectorizer(stop_words="english"), SVC(random_state=42))
         elif self.model == "xgboost":
@@ -120,10 +52,6 @@ class Models:
         class_report = classification_report(self.y_test, self.pred)
         return class_report
 
-    # def train_model(self):
-    #     self.clf.fit(self.X_train, self.y_train)
-        # return 
-
     def remove_punctuations(self, series):
         res = []
         for _ in series:
@@ -132,16 +60,10 @@ class Models:
 
     def save_fig(self):
         cm = confusion_matrix(self.y_test, self.pred)
-        # print(type(cm))
         display = ConfusionMatrixDisplay(cm, display_labels=self.clf.classes_).plot()
-        # display = ConfusionMatrixDisplay(cm).plot()
-        # display = ConfusionMatrixDisplay.from_predictions(self.y_test, self.pred).plot()
-        # print(type(display))
         plt.savefig("temp.png")
-        # plt.show()
 
     def final_result(self):
         self.model_init()
-        # self.train_model()
         self.metrics()
         self.save_fig()
